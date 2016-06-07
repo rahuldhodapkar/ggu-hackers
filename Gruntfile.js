e58c7e88da3d863e5680574b055bdf965dfd354c
rahuldhodapkar <rahul.m.dhodapkar@gmail.com>
2016-06-08 00:44:21 +0900

    using port environment variable

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,7 +70,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 3000,
+        port: process.env.PORT,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '0.0.0.0',
         livereload: 35729
