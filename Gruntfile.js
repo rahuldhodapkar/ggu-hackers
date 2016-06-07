efb0cdbe28307015b1ed765135c471dcac60a25d
rahuldhodapkar <rahul.m.dhodapkar@gmail.com>
2016-06-08 00:40:26 +0900

    remove wiredep

--- Gruntfile.js
+++ Gruntfile.js
@@ -434,7 +434,7 @@ module.exports = function (grunt) {
 
     grunt.task.run([
       'clean:server',
-      'wiredep',
+      //'wiredep',
       'concurrent:server',
       'postcss:server',
       'connect:livereload',
